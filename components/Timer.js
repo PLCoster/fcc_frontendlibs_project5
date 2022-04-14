bc06885f42d4adece02a9c00191de863da84735d
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-04-13 23:36:50 -0700

    Basic functionality of pomodoro timer completed:
    - Timer can be started / stopped (paused) and reset
    - Session and Break lengths can be adjusted between 1 and 60 mins
    - Ending a timer phase will begin the new timer phase starting with specified time
    - Timer label switches as timer phases change
    
    Still requires styling, audio elements added, tests and optional additional
    functionality e.g. disable auto-begin next phase, tasks, session history

--- components/Timer.js
+++ components/Timer.js
@@ -1,39 +1,115 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 
 function Timer() {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
+  const [breakLengthMins, setBreakLengthMins] = useState(5);
+  const [sessionLengthMins, setSessionLengthMins] = useState(25);
 
   const [timerRunning, setTimerRunning] = useState(false);
-  const [timerStartTime, setTimerStartTime] = useState(null);
-  const [currentTimerSeconds, setCurrentTimer] = useState(300);
+  const [timerSecondsRemaining, setTimerSecondsRemaining] = useState(1500);
+  const timerSecondsRemainingRef = useRef(1500); // Ref required to access state inside timeout
+  const [currTimeoutID, setCurrTimeoutID] = useState(null);
 
+  const [timerPhase, setTimerPhase] = useState('Session');
+  const [swapPhase, setSwapPhase] = useState(false);
+
+  // Handler to reset timer to initial state when Reset button is clicked
   const handleResetClick = () => {
-    setBreakLength(5);
-    setSessionLength(25);
+    setBreakLengthMins(5);
+    setSessionLengthMins(25);
+    setTimerRunning(false);
+    setTimerSecondsRemaining(1500);
+    timerSecondsRemainingRef.current = 1500;
+    setTimerPhase('Session');
+    setSwapPhase(false);
   };
 
   // Handler to control updates to session and break length
-  const handleTimerLengthChange = (increment, timerValue, timerSetter) => {
-    const newTimerValue = timerValue + increment;
+  const handleTimerLengthChange = (increment, sessionName) => {
+    const timerMins =
+      sessionName === 'Session' ? sessionLengthMins : breakLengthMins;
+    const setter =
+      sessionName === 'Session' ? setSessionLengthMins : setBreakLengthMins;
 
-    if (newTimerValue > 0 && newTimerValue < 61) {
-      timerSetter(newTimerValue);
+    const newTimerMins = timerMins + increment;
+
+    if (newTimerMins > 0 && newTimerMins < 61) {
+      setter(newTimerMins);
+
+      // Update current phase timer if timer is not running:
+      if (!timerRunning && timerPhase === sessionName) {
+        const newTimerSeconds = newTimerMins * 60;
+        setTimerSecondsRemaining(newTimerSeconds);
+        timerSecondsRemainingRef.current = newTimerSeconds;
+      }
     }
   };
 
   const handleTimerStopStart = () => {
+    console.log('handleTimerStopStart Trigerred');
     const newTimerRunning = !timerRunning;
 
-    // Start timer when switched on
+    // Start timer when switched on, store timeout cancel function
     if (newTimerRunning) {
       console.log('Starting timer at: ', Date.now());
+      handleTimerStart();
+    }
+
+    // When timer switched off, cancel current timeout:
+    else {
+      console.log('Stopping timer at: ', Date.now());
+      clearTimeout(currTimeoutID);
     }
 
     setTimerRunning(newTimerRunning);
   };
+
+  // Handler function to decrement timer seconds every second using setTimeout
+  // More accurate timekeeping than setInterval or repeated 1000ms setTimeouts
+  const handleTimerStart = () => {
+    console.log('Setting up new timer!');
+    let nextTickTime = Date.now() + 1000;
+    let timeoutID = null;
+
+    const timeoutFunction = () => {
+      // Decrement Seconds Remaining Ref, use this to set the Seconds Remaining State
+      timerSecondsRemainingRef.current -= 1;
+
+      // Continue countdown on timer until it hits 0:
+      if (timerSecondsRemainingRef.current >= 0) {
+        setTimerSecondsRemaining(timerSecondsRemainingRef.current);
+        nextTickTime += 1000;
+        timeoutID = setTimeout(timeoutFunction, nextTickTime - Date.now());
+        setCurrTimeoutID(timeoutID);
+      } else {
+        // When timer reaches 0, swap timer phase e.g. Session -> Break
+        setSwapPhase(true);
+      }
+    };
+
+    timeoutID = setTimeout(timeoutFunction, nextTickTime - Date.now());
+    setCurrTimeoutID(timeoutID);
+  };
+
+  // Effect to handle swapping phase from e.g. Session -> Break when timer reaches 0
+  useEffect(() => {
+    if (swapPhase) {
+      // Cancel current timeout
+      clearTimeout(currTimeoutID);
+
+      // Swap Timer Phase
+      const newPhase = timerPhase === 'Session' ? 'Break' : 'Session';
+      setTimerPhase(newPhase);
+
+      // Start new timer
+      timerSecondsRemainingRef.current =
+        (newPhase === 'Session' ? sessionLengthMins : breakLengthMins) * 60;
+      setTimerSecondsRemaining(timerSecondsRemainingRef.current);
+      handleTimerStart();
+      setSwapPhase(false);
+    }
+  }, [swapPhase]);
 
   return (
     <>
@@ -45,17 +121,17 @@ function Timer() {
           id="break-decrement"
           onClick={() => {
             console.log('decrementing break ');
-            handleTimerLengthChange(-1, breakLength, setBreakLength);
+            handleTimerLengthChange(-1, 'Break');
           }}
         >
           <i className="bi bi-arrow-down-circle-fill" />
         </Button>
-        <span id="break-length">{breakLength}</span>
+        <span id="break-length">{breakLengthMins}</span>
         <Button
           id="break-increment"
           onClick={() => {
             console.log('incrementing break ');
-            handleTimerLengthChange(1, breakLength, setBreakLength);
+            handleTimerLengthChange(1, 'Break');
           }}
         >
           <i className="bi bi-arrow-up-circle-fill" />
@@ -67,18 +143,14 @@ function Timer() {
         Session Length:
         <Button
           id="session-decrement"
-          onClick={() =>
-            handleTimerLengthChange(-1, sessionLength, setSessionLength)
-          }
+          onClick={() => handleTimerLengthChange(-1, 'Session')}
         >
           <i className="bi bi-arrow-down-circle-fill" />
         </Button>
-        <span id="session-length">{sessionLength}</span>
+        <span id="session-length">{sessionLengthMins}</span>
         <Button
           id="session-increment"
-          onClick={() =>
-            handleTimerLengthChange(1, sessionLength, setSessionLength)
-          }
+          onClick={() => handleTimerLengthChange(1, 'Session')}
         >
           <i className="bi bi-arrow-up-circle-fill" />
         </Button>
@@ -86,16 +158,16 @@ function Timer() {
 
       {/* TIMER */}
       <div className="timer-clock">
-        <h4 id="timer-label">Session</h4>
+        <h4 id="timer-label">{timerPhase}</h4>
         <h2 id="time-left">
-          {timerRunning
-            ? `${Math.floor(currentTimerSeconds / 60)}:${
-                currentTimerSeconds - 60 * Math.floor(currentTimerSeconds / 60)
-              }`
-            : `${sessionLength}:00`}
+          {`${Math.floor(timerSecondsRemaining / 60)
+            .toString()
+            .padStart(2, '0')}:${(timerSecondsRemaining % 60)
+            .toString()
+            .padStart(2, '0')}`}
         </h2>
         <Button id="start_stop" onClick={handleTimerStopStart}>
-          Start / Stop
+          {timerRunning ? 'Stop' : 'Start'}
         </Button>
         <Button id="reset" onClick={handleResetClick}>
           Reset
