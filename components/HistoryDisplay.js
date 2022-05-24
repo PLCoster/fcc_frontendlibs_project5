9a2fcbd64196bb602ab12fe5e01ae97937435e4b
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-05-24 15:12:01 -0700

    Updated styling and display of detailed history display
    Detailed history now displays individual completed sessions
    in a card-like format

--- components/HistoryDisplay.js
+++ components/HistoryDisplay.js
@@ -1,9 +1,9 @@
 import { useState, useMemo } from 'react';
 
 // import Head from 'next/head';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 
-import styles from './styles/Timer.module.css';
+import styles from './styles/HistoryDisplay.module.css';
 import buttonStyles from './styles/TimerButtons.module.css';
 
 function HistoryDisplay({ timerPhase, history, handleClearHistoryClick }) {
@@ -24,17 +24,61 @@ function HistoryDisplay({ timerPhase, history, handleClearHistoryClick }) {
     );
   }, [history]);
 
+  const millisecondsToMinutes = (ms) => {
+    return Math.floor(ms / (1000 * 60));
+  };
+
   // Memoised creation of detailed Session History
   const phaseHistory = useMemo(() => {
     return history.map(([phaseName, phaseSecs, phaseStart, phaseEnd]) => {
       return (
-        <li>
-          {phaseName}: StartTime:{' '}
-          {new Date(phaseStart).toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
-          })}
-        </li>
+        <Row
+          className={`${styles.historyItem} ${
+            phaseName === 'Session'
+              ? buttonStyles.onSessionFont
+              : buttonStyles.onBreakFont
+          } m-1 text-start`}
+        >
+          <Col xs={2}>
+            <div
+              className={`${styles.historyItemTitle} d-flex flex-column justify-content-center`}
+            >
+              {' '}
+              <h6>{phaseName}</h6>
+            </div>
+          </Col>
+
+          <Col xs={4}>
+            <div className="d-flex flex-column align-items-start">
+              <p className="m-1 small">
+                Start:{' '}
+                {new Date(phaseStart).toLocaleTimeString([], {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
+              </p>
+              <p className="m-1 small">
+                End:{' '}
+                {new Date(phaseEnd).toLocaleTimeString([], {
+                  hour: '2-digit',
+                  minute: '2-digit',
+                })}
+              </p>
+            </div>
+          </Col>
+
+          <Col xs={6}>
+            <div className="d-flex align items-center flex-column">
+              <p className="m-1 small">
+                Actual Duration: {millisecondsToMinutes(phaseEnd - phaseStart)}{' '}
+                min(s)
+              </p>
+              <p className="m-1 small">
+                Timed Duration: {Math.floor(phaseSecs / 60)} min(s)
+              </p>
+            </div>
+          </Col>
+        </Row>
       );
     });
   }, [history]);
@@ -93,11 +137,11 @@ function HistoryDisplay({ timerPhase, history, handleClearHistoryClick }) {
       </Row>
 
       {displayDetailedHistory ? (
-        <Row className="mt-3">
-          <Col>
-            <ul>{phaseHistory.length ? phaseHistory : 'No History Yet!'}</ul>
-          </Col>
-        </Row>
+        <div
+          className={`${styles.detailedHistoryContainer} d-flex flex-column align-items-center mt-3 mb-3`}
+        >
+          {phaseHistory.length ? phaseHistory.reverse() : 'No History Yet!'}
+        </div>
       ) : null}
     </>
   );
