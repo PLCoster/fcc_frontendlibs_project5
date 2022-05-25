3dc3a2c5cde5a26576542d86af56aaf2e079af3e
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-05-25 12:27:51 -0700

    Added basePath and assetPrefix to nextConfig for gh-pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+// basePath and assetPrefix are needed for gh-pages deployment
+// otherwise paths to assets are not set correctly
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
