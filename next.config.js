3082da9cdaa19b904f6c0f60bb3b9490bc00f180
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-05-25 12:31:01 -0700

    Added extra info links for nextConfig basePath/assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 
 // basePath and assetPrefix are needed for gh-pages deployment
 // otherwise paths to assets are not set correctly
+// https://nextjs.org/docs/api-reference/next.config.js/cdn-support-with-asset-prefix
+// https://nextjs.org/docs/api-reference/next.config.js/basepath
 
 const nextConfig = {
   reactStrictMode: true,
