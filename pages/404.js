15996fb0b02d7556e9e2dccbea19d1a53395ad4b
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-05-25 17:53:38 -0700

    Added a custom 404 page to redirect to the application root

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+
+import Error from 'next/error';
+import { useRouter } from 'next/router';
+
+export default function Custom404() {
+  const router = useRouter();
+
+  // Redirect to the app
+  // useEffect(() => {
+  //   router.replace('/');
+  // });
+
+  // Note this behaviour could possibly be achieved with next.js redirects
+  // https://nextjs.org/docs/api-reference/next.config.js/redirects
+
+  return (
+    <Error statusCode={404} title="This page does not exist! Redirecting.." />
+  );
+}
